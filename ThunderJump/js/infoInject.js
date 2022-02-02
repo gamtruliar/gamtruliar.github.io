63375fe3dba0d97ad773addf35ec19eff20e9601
gamtruliar <gamtruliar@gmail.com>
2022-02-03 05:08:01 +0800

    reverse

--- ThunderJump/js/infoInject.js
+++ ThunderJump/js/infoInject.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
 pstr+="如果對本作中的幸/Thunder有興趣, 歡迎去到 榎川幸【香港VTuber】 的 Youtube Channel </br>";
 pstr+="<a href='https://www.youtube.com/channel/UCQa-7tpKlLW3pnPGAyMPgsA'>傳送門</a> </br>";
 pstr+="</div>";
-pstr+="<div> 版本記錄:V1.0.2 語言選擇</br>";
+pstr+="<div> 版本記錄:V1.0.1 開放測試</br>";
 pstr+="</div>";
 $("#infoInject").html(pstr);
 
