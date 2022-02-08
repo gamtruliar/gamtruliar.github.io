b5d7bd556233976df95a8a2a1255a52a5655df48
gamtruliar <gamtruliar@gmail.com>
2022-02-08 20:41:33 +0800

    android download link

--- ThunderJump/js/infoInject.js
+++ ThunderJump/js/infoInject.js
@@ -2,7 +2,8 @@
 
 $( document ).ready(function() {
 	var pstr="<div> 本作不是官方出版, 只作fan game試玩用</br>";
-pstr+="如果對本作中的幸/Thunder有興趣, 歡迎去到 榎川幸【香港VTuber】 的 Youtube Channel </br>";
+pstr+="<a href='https://play.google.com/store/apps/details?id=com.NwcsGame.ThunderJump&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='立即下載 Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/zh-hk_badge_web_generic.png'/></a></br>";	
+pstr+="如果對本作中的幸/Thunder有興趣, </br>歡迎去到 榎川幸【香港VTuber】 的 Youtube Channel </br>";
 pstr+="<a href='https://www.youtube.com/channel/UCQa-7tpKlLW3pnPGAyMPgsA'>傳送門</a> </br>";
 pstr+="</div>";
 pstr+="<div> 版本記錄:V1.0.2 語言選擇</br>";
