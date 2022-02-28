143034458ed7198f79479a2e181084cf6f6e7c1b
gamtruliar <gamtruliar@gmail.com>
2022-02-28 09:45:24 +0800

    domain change

--- ThunderJump/js/infoInject.js
+++ ThunderJump/js/infoInject.js
@@ -6,7 +6,7 @@ pstr+="<a href='https://play.google.com/store/apps/details?id=com.NwcsGame.Thund
 pstr+="如果對本作中的幸/Thunder有興趣, </br>歡迎去到 榎川幸【香港VTuber】 的 Youtube Channel </br>";
 pstr+="<a href='https://www.youtube.com/channel/UCQa-7tpKlLW3pnPGAyMPgsA'>傳送門</a> </br>";
 pstr+="</div>";
-pstr+="<div> 版本記錄:V1.0.2 語言選擇</br>";
+pstr+="<div> 版本記錄:V1.0.3 domain fix</br>";
 pstr+="</div>";
 $("#infoInject").html(pstr);
 
